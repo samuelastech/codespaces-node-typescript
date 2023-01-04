836c6de240cc61da0d520d0518a6e1a3c01da7c8
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-04 15:55:12 -0300

    chore: remove coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -17,13 +17,13 @@ export default {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  // coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -121,7 +121,7 @@ export default {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: './src',
+  rootDir: './',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
